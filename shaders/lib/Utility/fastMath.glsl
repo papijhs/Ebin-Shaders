3ef0f5f62e864013d9076ce9d550c7d49a916a33
dotModded <dotmodded@gmail.com>
2016-09-20 13:59:34 -0700

    added a new define

--- shaders/lib/Utility/fastMath.glsl
+++ shaders/lib/Utility/fastMath.glsl
@@ -1,25 +1,16 @@
+//These functions guarentee the driver will always use the fastest avalible method for intense computations and approximations, even if these do not affect performance
+//On some hardware it at least guarentees that the driver can never break performance.
 // [Drobot2014a] Low Level Optimisations for GNC
 #define fsqrt(x) intBitsToFloat(0x1FBD1DF5 + (floatBitsToInt(x) >> 1)) // Error of 1.42% Literally Free
 
 #define finversesqrt(x) intBitsToFloat(0x5F33E79F - (floatBitsToInt(x) >> 1)) // Error of 1.62%
 
-
-float flength(vec2 x) {
-	return fsqrt(dot(x, x));
-}
-
-float flength(vec3 x) {
-	return fsqrt(dot(x, x));
-}
-
-float flength(vec4 x) {
-	return fsqrt(dot(x, x));
-}
-
+#define flength(x) fsqrt(dot(x, x))
 
 float facos(float x) { // No matrix with under 3% error
 	// [Eberly2014] GPGPU Programming for Games and Science
-	float res = -0.156583 * abs(x) + PI * 0.5;
-	res *= fsqrt(1.0 - abs(x));
+	float ax = abs(x);
+	float res = -0.156583 * ax + PI * 0.5;
+	res *= fsqrt(1.0 - ax);
 	return x >= 0 ? res : PI - res;
 }
