70fd8430c496d67e78f50273994b526218c3b3a4
BruceKnowsHow <bruceknowshow@ymail.com>
2016-09-27 20:11:55 -0700

    Restructured custom time cycles so that it takes 2 uniform #include declarations and a vertex setup()

--- shaders/lib/Uniform/Global_Composite_Variables.glsl
+++ shaders/lib/Uniform/Global_Composite_Variables.glsl
@@ -1,15 +1,3 @@
-#if defined fsh
-	#ifdef CUSTOM_TIME_CYCLE
-		varying mat4 shadowView;
-		
-		#define shadowViewMatrix shadowView
-	#else
-		uniform mat4 shadowModelView;
-		
-		#define shadowViewMatrix shadowModelView
-	#endif
-#endif
-
 varying vec3 lightVector;
 varying vec3 worldLightVector;
 varying vec3 sunVector;
